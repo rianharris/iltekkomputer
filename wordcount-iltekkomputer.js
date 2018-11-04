c1c159c6bab8611efc604956353c5d6e62c53d6c
rianharris <44161064+rianharris@users.noreply.github.com>
2018-11-05 00:02:10 +0700

    Update wordcount-iltekkomputer.js

--- wordcount-iltekkomputer.js
+++ wordcount-iltekkomputer.js
@@ -1,8 +1,5 @@
 var widgetHtml = '<iframe src="https://www.prepostseo.com/widget/wordcount" style="border:0;width:100%;min-height:1000px;height:100%;"></iframe>';
 var widgetTag = document.getElementById("ppsLink");
-if (widgetTag === null){ 
-	
-}
 
 var isNew = document.getElementById("ppsWidgetCode");
 
